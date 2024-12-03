192c1c768a8a02c1420b3a377468a8fce18cbfb7
David Ohhana <sustujbv167@gmail.com>
2024-12-03 15:14:05 +0200

    with algo

--- src/main/java/com/handson/searchengine/controller/AppController.java
+++ src/main/java/com/handson/searchengine/controller/AppController.java
@@ -1,7 +1,16 @@
 package com.handson.searchengine.controller;
 
 
+import java.io.IOException;
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import com.handson.searchengine.crawler.Crawler;
+import com.handson.searchengine.model.CrawlStatus;
+import com.handson.searchengine.model.CrawlStatusOut;
+import com.handson.searchengine.model.CrawlerRequest;
 
 @RestController
 @RequestMapping("/api")
@@ -11,4 +20,28 @@ public class AppController {
     public String hello() {
         return "Hello";
     }
+
+        private static final int ID_LENGTH = 6;
+private Random random = new Random();
+@Autowired
+    Crawler crawler;
+
+@RequestMapping(value = "/crawl", method = RequestMethod.POST)
+public CrawlStatusOut crawl(@RequestBody CrawlerRequest request) throws IOException, InterruptedException {
+        String crawlId = generateCrawlId();
+        if (!request.getUrl().startsWith("http")) {
+        request.setUrl("https://" + request.getUrl());
+        }
+        CrawlStatus res = crawler.crawl(crawlId, request);
+        return CrawlStatusOut.of(res);
+        }
+
+private String generateCrawlId() {
+        String charPool = "ABCDEFHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < ID_LENGTH; i++) {
+        res.append(charPool.charAt(random.nextInt(charPool.length())));
+        }
+        return res.toString();
+        }
 }
